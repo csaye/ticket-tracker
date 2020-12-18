45f242315f71a1e509b3e707fce7fe0243ead395
Cooper Saye <coopersaye@gmail.com>
2020-12-18 13:18:00 -0800

    Implement ticket opening.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
@@ -6,6 +6,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 const config = {
   apiKey: "AIzaSyCw5-tDfXVripCiIehoWt8MdbopY7Ug-wk",
@@ -31,8 +32,8 @@ function App() {
         <h1>Team Tracker</h1>
           { auth.currentUser && <SignOut /> }
       </header>
-      <section id="hellos">
-        { !auth.currentUser && <SignIn /> }
+      <section>
+        { auth.currentUser ? <TicketList /> : <SignIn /> }
       </section>
     </div>
   );
@@ -58,6 +59,60 @@ function SignOut() {
     <div className="SignOut">
       <p>Signed in as { auth.currentUser?.displayName } </p>
       <button onClick={() => auth.signOut()}>Sign Out</button>
+    </div>
+  );
+}
+
+// TicketList
+function TicketList() {
+
+  const ticketsRef = firestore.collection('tickets');
+  const query = ticketsRef.orderBy('createdAt').limit(25);
+
+  const [tickets] = useCollectionData(query, {idField: 'id'});
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const sendTicket = async(e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+
+    await ticketsRef.add({
+      title,
+      description,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setTitle('');
+    setDescription('');
+  }
+
+  return (
+    <div className="TicketList">
+      <div>
+        { tickets && tickets.map(tkt => <Ticket key={tkt.id} message={tkt} /> ) }
+      </div>
+      <form onSubmit={sendTicket}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} required />
+        <input value={description} onChange={(e) => setDescription(e.target.value)} required />
+        <button type="submit">Open Ticket</button>
+      </form>
+    </div>
+  );
+}
+
+// Ticket
+function Ticket(props) {
+  const { title, description, uid, photoURL } = props.message;
+
+  return (
+    <div className="Ticket">
+      <img src={photoURL} alt="" />
+      <h1>{title}</h1>
+      <p>{description}</p>
     </div>
   );
 }
