f588389e1ceea67c605368142583b3ba39a498f1
Cooper Saye <coopersaye@gmail.com>
2020-12-18 10:49:43 -0800

    Initialize firebase.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCw5-tDfXVripCiIehoWt8MdbopY7Ug-wk",
+  authDomain: "csaye-team-tracker.firebaseapp.com",
+  projectId: "csaye-team-tracker",
+  storageBucket: "csaye-team-tracker.appspot.com",
+  messagingSenderId: "268589792536",
+  appId: "1:268589792536:web:4a4954b1a06580d4beb26d"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Team Tracker</h1>
       </header>
     </div>
   );
